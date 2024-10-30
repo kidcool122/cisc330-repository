b190ad3508dae8e32d7815fcae797d8892a16b8d
Jace <jhannays@fordham.edu>
2024-10-30 17:45:39 -0400

    Hw 7 text and question 6&7 submission

---
+++ Hw-7-6&7.php
@@ -0,0 +1,31 @@
+<?php
+class User {
+    private $name;
+    private $email;
+
+    public function __construct($name, $email) {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public static function defaultUser() {
+        return new User("Default", "default@college.edu");
+    }
+}
+?>
